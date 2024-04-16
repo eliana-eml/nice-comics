d5bd6d0293d8ee379d5f9ba253d0f6a411834f9b
eliana-eml <eml.eliana57@hotmail.com>
2024-04-16 10:18:23 -0300

    Criação dos métodos PUT e DELETE na classe ClienteController

--- src/main/java/br/senac/nicecomics/nicecomics/controllers/ClienteController.java
+++ src/main/java/br/senac/nicecomics/nicecomics/controllers/ClienteController.java
@@ -6,9 +6,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -55,5 +57,18 @@ public class ClienteController {
         
         return ResponseEntity.ok(clienteEncontrado);
         
+    }
+    
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Cliente> atualizarCadastroCliente(@RequestBody Cliente cliente) {
+        
+        Cliente clienteAlterado = clienteRepository.save(cliente);
+        return ResponseEntity.ok(clienteAlterado);
+        
+    }
+    
+    @DeleteMapping(value = "/{id}")
+    public void excluirCadastroCliente(@PathVariable Long id) {
+        clienteRepository.deleteById(id);
     }
 }
