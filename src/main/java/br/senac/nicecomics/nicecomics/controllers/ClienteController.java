719e9122075effb6dedbbd76aaafbf5bba6ee7ae
Nathalia Narumi <91766720+nathalianarumi@users.noreply.github.com>
2024-04-15 21:23:02 -0300

    Criação de método get para retorno de um cliente referenciando o id do mesmo

--- src/main/java/br/senac/nicecomics/nicecomics/controllers/ClienteController.java
+++ src/main/java/br/senac/nicecomics/nicecomics/controllers/ClienteController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,6 +25,12 @@ public class ClienteController {
     public List<Cliente> listarClientes() {
         List<Cliente> listaClientes = clienteRepository.findAll();
         return listaClientes;
+    }
+    
+    @GetMapping(value = "/{id}")
+    public Cliente findById(@PathVariable Long id) {
+        Cliente result = clienteRepository.findById(id).get();
+        return result;
     }
     
     @PostMapping
